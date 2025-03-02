13880aa56cd952c7b76693e90f22c71bc85e94d6
Vidushi Meel <meelvidushi1@gmail.com>
2025-03-01 20:34:00 -0500

    fix: all tests passing, fix build_tree(t)

--- part1.py
+++ part1.py
@@ -1,14 +1,26 @@
 import math
 import numpy as np
 from collections import Counter
-
-#-----------------------------------------------
 class Node:
-    def __init__(self, X, Y, i=None, C=None, isleaf=False, p=None):
+    '''
+        Decision Tree Node (with discrete attributes)
+        Inputs: 
+            X: the data instances in the node, a numpy matrix of shape p by n.
+               Each element can be int/float/string.
+               Here n is the number data instances in the node, p is the number of attributes.
+            Y: the class labels, a numpy array of length n.
+               Each element can be int/float/string.
+            i: the index of the attribute being tested in the node, an integer scalar 
+            C: the dictionary of attribute values and children nodes. 
+               Each (key, value) pair represents an attribute value and its corresponding child node.
+            isleaf: whether or not this node is a leaf node, a boolean scalar
+            p: the label to be predicted on the node (i.e., most common label in the node).
+    '''
+    def __init__(self,X,Y, i=None,C=None, isleaf= False,p=None):
         self.X = X
         self.Y = Y
         self.i = i
-        self.C = C
+        self.C= C
         self.isleaf = isleaf
         self.p = p
 
@@ -22,6 +34,11 @@ class Tree(object):
     def entropy(Y):
         '''
             Compute the entropy of a list of values.
+            Input:
+                Y: a list of values, a numpy array of int/float/string values.
+            Output:
+                e: the entropy of the list of values, a float scalar
+            Hint: you could use collections.Counter.
         '''
         counter = Counter(Y)
         total = len(Y)
@@ -32,7 +49,12 @@ class Tree(object):
     @staticmethod
     def conditional_entropy(Y, X):
         '''
-            Compute the conditional entropy of Y given X.
+            Compute the conditional entropy of y given x. The conditional entropy H(Y|X) means average entropy of children nodes, given attribute X. Refer to https://en.wikipedia.org/wiki/Information_gain_in_decision_trees
+            Input:
+                X: a list of values , a numpy array of int/float/string values. The size of the array means the number of instances/examples. X contains each instance's attribute value. 
+                Y: a list of values, a numpy array of int/float/string values. Y contains each instance's corresponding target label. For example X[0]'s target label is Y[0]
+            Output:
+                ce: the conditional entropy of y given x, a float scalar
         '''
         counter = Counter(X)
         total = len(X)
@@ -43,7 +65,13 @@ class Tree(object):
     @staticmethod
     def information_gain(Y, X):
         '''
-            Compute the information gain of Y after splitting over attribute X.
+            Compute the information gain of y after spliting over attribute x
+            InfoGain(Y,X) = H(Y) - H(Y|X) 
+            Input:
+                X: a list of values, a numpy array of int/float/string values.
+                Y: a list of values, a numpy array of int/float/string values.
+            Output:
+                g: the information gain of y after spliting over x, a float scalar
         '''
         return Tree.entropy(Y) - Tree.conditional_entropy(Y, X)
 
@@ -51,7 +79,16 @@ class Tree(object):
     @staticmethod
     def best_attribute(X, Y):
         '''
-            Find the best attribute to split the node.
+            Find the best attribute to split the node. 
+            Here we use information gain to evaluate the attributes. 
+            If there is a tie in the best attributes, select the one with the smallest index.
+            Input:
+                X: the feature matrix, a numpy matrix of shape p by n. 
+                   Each element can be int/float/string.
+                   Here n is the number data instances in the node, p is the number of attributes.
+                Y: the class labels, a numpy array of length n. Each element can be int/float/string.
+            Output:
+                i: the index of the attribute to split, an integer scalar
         '''
         gains = [Tree.information_gain(Y, X[i]) for i in range(X.shape[0])]
         return np.argmax(gains)
@@ -60,7 +97,22 @@ class Tree(object):
     @staticmethod
     def split(X, Y, i):
         '''
-            Split the node based on the i-th attribute.
+            Split the node based upon the i-th attribute.
+            (1) split the matrix X based upon the values in i-th attribute
+            (2) split the labels Y based upon the values in i-th attribute
+            (3) build children nodes by assigning a submatrix of X and Y to each node
+            (4) build the dictionary to combine each  value in the i-th attribute with a child node.
+    
+            Input:
+                X: the feature matrix, a numpy matrix of shape p by n.
+                   Each element can be int/float/string.
+                   Here n is the number data instances in the node, p is the number of attributes.
+                Y: the class labels, a numpy array of length n.
+                   Each element can be int/float/string.
+                i: the index of the attribute to split, an integer scalar
+            Output:
+                C: the dictionary of attribute values and children nodes. 
+                   Each (key, value) pair represents an attribute value and its corresponding child node.
         '''
         values = np.unique(X[i])
         C = {}
@@ -73,15 +125,27 @@ class Tree(object):
     @staticmethod
     def stop1(Y):
         '''
-            Stop condition: all instances have the same label.
+            Test condition 1 (stop splitting): whether or not all the instances have the same label. 
+    
+            Input:
+                Y: the class labels, a numpy array of length n.
+                   Each element can be int/float/string.
+            Output:
+                s: whether or not Conidtion 1 holds, a boolean scalar. 
+                True if all labels are the same. Otherwise, false.
         '''
         return len(set(Y)) == 1
-
     #--------------------------
     @staticmethod
     def stop2(X):
         '''
-            Stop condition: all instances have the same attribute values.
+            Test condition 2 (stop splitting): whether or not all the instances have the same attribute values. 
+            Input:
+                X: the feature matrix, a numpy matrix of shape p by n.
+                   Each element can be int/float/string.
+                   Here n is the number data instances in the node, p is the number of attributes.
+            Output:
+                s: whether or not Conidtion 2 holds, a boolean scalar. 
         '''
         return np.all(X == X[:, [0]])
 
@@ -89,7 +153,13 @@ class Tree(object):
     @staticmethod
     def most_common(Y):
         '''
-            Get the most-common label from the list Y.
+            Get the most-common label from the list Y. 
+            Input:
+                Y: the class labels, a numpy array of length n.
+                   Each element can be int/float/string.
+                   Here n is the number data instances in the node.
+            Output:
+                y: the most common label, a scalar, can be int/float/string.
         '''
         return Counter(Y).most_common(1)[0][0]
 
@@ -98,18 +168,27 @@ class Tree(object):
     def build_tree(t):
         '''
             Recursively build tree nodes.
+            Input:
+                t: a node of the decision tree, without the subtree built.
+                t.X: the feature matrix, a numpy float matrix of shape p by n.
+                   Each element can be int/float/string.
+                    Here n is the number data instances, p is the number of attributes.
+                t.Y: the class labels of the instances in the node, a numpy array of length n.
+                t.C: the dictionary of attribute values and children nodes. 
+                   Each (key, value) pair represents an attribute value and its corresponding child node.
         '''
+
         if Tree.stop1(t.Y):
             t.isleaf = True
             t.p = t.Y[0]
             return
-
+        
         if Tree.stop2(t.X):
             t.isleaf = True
             t.p = Tree.most_common(t.Y)
             return
-
         t.i = Tree.best_attribute(t.X, t.Y)
+        t.p = Tree.most_common(t.Y)
         t.C = Tree.split(t.X, t.Y, t.i)
 
         for child in t.C.values():
@@ -119,7 +198,15 @@ class Tree(object):
     @staticmethod
     def train(X, Y):
         '''
-            Train a decision tree.
+            Given a training set, train a decision tree. 
+            Input:
+                X: the feature matrix, a numpy matrix of shape p by n.
+                   Each element can be int/float/string.
+                   Here n is the number data instances in the training set, p is the number of attributes.
+                Y: the class labels, a numpy array of length n.
+                   Each element can be int/float/string.
+            Output:
+                t: the root of the tree.
         '''
         root = Node(X, Y)
         Tree.build_tree(root)
